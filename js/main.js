1d146ad1c8ad923019a2e14df898062ed0ea9d7d
Jasur <developer7102@gmail.com>
2021-07-04 18:41:28 +0200

    xatolar togirlanayapti

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-var elHeaderSection = document.querySelector('.header-section')
+var elHeaderSection = document.querySelector('.header')
 var elHeaderButton = document.querySelector('.js-header-button');
 
 
-if(elHeaderButton){
-  elHeaderButton.addEventListener('click', function() {
+if (elHeaderButton) {
+  elHeaderButton.addEventListener('click', function () {
     elHeaderSection.classList.toggle('header-section--open');
   });
 }
+
+
+// accordion
+
+let elsAccordionButton = document.querySelectorAll('.how__drink--button');
+
+elsAccordionButton.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.closest('.how__drink').classList.toggle('how__drink--open')
+  })
+})
