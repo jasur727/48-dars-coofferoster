a46e3c7de20645dc48be738bdad5e74e649d8ce2
Jasur <developer7102@gmail.com>
2021-07-09 05:33:30 -0700

    add modal change

--- js/main.js
+++ js/main.js
@@ -16,5 +16,19 @@ let elsAccordionButton = document.querySelectorAll('.how__drink--button');
 elsAccordionButton.forEach(btn => {
   btn.addEventListener('click', () => {
     btn.closest('.how__drink').classList.toggle('how__drink--open')
-  })
+  });
+});
+
+
+var elBtnCreatePlan = document.querySelector(".create__plan");
+var elOrderModal = document.querySelector('.order-modal');
+elBtnCreatePlan.addEventListener("click", function () {
+  elOrderModal.classList.add('order-modal--active');
+})
+
+var elOrderModalActiveSection = document.querySelector(".order-modal--active");
+
+
+elOrderModal.addEventListener("click", function () {
+    elOrderModal.classList.remove("order-modal--active");
 })
